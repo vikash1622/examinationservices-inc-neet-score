a3fd62b1d328cdf5ea356ddc956d3b08c2013734
vikash1622 <155430726+vikash1622@users.noreply.github.com>
2026-07-21 12:10:31 +0530

    Update total marks from 535 to 545 in PercentileTable

--- src/componentnts/PercentileTable.jsx
+++ src/componentnts/PercentileTable.jsx
@@ -54,7 +54,7 @@ const PercentileTable = () => {
                         <p className="s1" style={{ paddingTop: '1pt', paddingLeft: '1pt', textIndent: '0pt', textAlign: 'left' }}>Total marks obtained (out of 720)</p>
                     </td>
                     <td style={{ width: '374pt', border: '1pt solid #CCCCCC' }} colSpan="4">
-                        <p className="s2" style={{ paddingTop: '2pt', paddingLeft: '1pt', textIndent: '0pt', textAlign: 'left' }}>535</p>
+                        <p className="s2" style={{ paddingTop: '2pt', paddingLeft: '1pt', textIndent: '0pt', textAlign: 'left' }}>545</p>
                     </td>
                 </tr>
                 <tr style={{ height: '16pt' }}>
